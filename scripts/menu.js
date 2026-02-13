43f785ce277fe2e6abb63445cecaaefc16d241c4
Jonathan Matos <jonathan2018.matos@gmail.com>
2026-02-13 13:28:47 -0300

    estilização do contato e footer finalizadas

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,12 +1,27 @@
 const btnMobile = document.querySelector(".menu-icon");
+const btnIcon = btnMobile.querySelector("img");
 
 function toggleMenu(event) {
   if (event.type === "touchstart") event.preventDefault();
   const nav = document.querySelector(".menu-mobile");
-  nav.classList.toggle("active");
+  const isOpen = nav.classList.toggle("active");
+
+  const open = "assets/icons/menu.svg";
+  const close = "assets/icons/close.svg";
+
+  const newSrc = isOpen ? close : open;
+
+  btnIcon.style.opacity = "0";
+  btnIcon.style.transform = "scale(0.85)";
+
+  setTimeout(() => {
+    btnIcon.src = newSrc + "?t=" + Date.now();
+    btnIcon.style.opacity = "1";
+    btnIcon.style.transform = "scale(1)";
+  }, 200);
 }
 
-btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
 
 const themeToggle = document.querySelectorAll(".theme-mode");
 const body = document.body;
@@ -16,11 +31,9 @@ const moonIconPath = "assets/icons/moon.svg";
 //Troca tema
 function trocarTema(tipo) {
   if (tipo === true) {
-    console.log("Tema claro ativado");
     body.classList.add("light");
   } else {
     body.classList.remove("light");
-    console.log("Tema escuro ativado");
   }
 
   const iconImages = document.querySelectorAll(".theme-icon img");
